b5974b65fbf5d410d44d0daec90ce6e59289384a
Belal-Sh <belal.she7adeh@outlook.com>
2021-11-20 23:37:41 +0200

    project created

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  return ( <
+    div className = "ui container comments" >
+    <
+    ApprovalCard >
+    <
+    CommentDetail author = "Sam"
+    timeAgo = " Today at 4:00am"
+    commentText = "ok!"
+    avatar = {
+      faker.image.avatar()
+    }
+    / > < /
+    ApprovalCard > <
+    ApprovalCard > <
+    CommentDetail author = "alex"
+    timeAgo = " Today at 5:00am"
+    commentText = "hello!"
+    avatar = {
+      faker.image.avatar()
+    }
+    / ></ApprovalCard >
+    <
+    ApprovalCard > <
+    CommentDetail author = "ali"
+    timeAgo = " Today at 1:00am"
+    commentText = "Nice!"
+    avatar = {
+      faker.image.avatar()
+    }
+    / ></ApprovalCard >
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+    <
+    /div>
+
+  );
+};
+
+ReactDOM.render( < App / > , document.querySelector('#root'))
